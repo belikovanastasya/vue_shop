b805f8986c07f4784b6c7726d5788c316ca251f0
belikovanastasya <belikova.ava@gmail.com>
2019-03-18 07:58:04 +0200

    crate base component

--- src/main.js
+++ src/main.js
@@ -3,15 +3,16 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import VeeValidate from 'vee-validate';
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
+Vue.use(BootstrapVue);
+Vue.use(VeeValidate);
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  template: '<App />',
-  components: {
-    App,
-  },
-});
+new Vue({el: '#app', router, template: '<App />', components: {
+    App
+  }});
