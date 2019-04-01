bf179868b78032ae94cf67509cf6d7ac0c5bb165
andrey.yatsenko <andrey.yatsenko@codeit.com.ua>
2019-04-01 18:28:42 +0300

    Create: store

--- src/main.js
+++ src/main.js
@@ -1,18 +1,23 @@
 // The Vue build version to load with the `import` command (runtime-only or
 // standalone) has been set in webpack.base.conf with an alias.
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import VeeValidate from 'vee-validate';
+import BootstrapVue from 'bootstrap-vue';
 import Vue from 'vue';
 import App from './App';
 import router from './router';
-import VeeValidate from 'vee-validate';
-import BootstrapVue from 'bootstrap-vue';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+import store from './store';
 
 Vue.use(BootstrapVue);
 Vue.use(VeeValidate);
 Vue.config.productionTip = false;
+Vue.config.devtools = process.env.NODE_ENV === 'development';
+
 
 /* eslint-disable no-new */
-new Vue({el: '#app', router, template: '<App />', components: {
+new Vue({el: '#app', router, store, template: '<App />', components: {
     App
   }});
+
+window.__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue = app.constructor;
